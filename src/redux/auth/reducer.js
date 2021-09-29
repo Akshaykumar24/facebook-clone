5bffe750a526ee4706d107683906a72f75dc5f59
Akshaykumar <akshaykumarpurad99@gmail.com>
2021-09-30 02:36:48 +0530

    all kind of requests

---
+++ src/redux/auth/reducer.js
@@ -0,0 +1,58 @@
+import {
+  LOG_FAILURE,
+  LOG_REQUEST,
+  LOG_SUCCESS,
+  REG_FAILURE,
+  REG_REQUEST,
+  REG_SUCCESS,
+} from "./actionTypes";
+
+const init = { user: {}, Load: false, Error: false, token: "", reg: false };
+
+export const authReducer = (state = { ...init }, { type, payload }) => {
+  //console.log(state, "users");
+  switch (type) {
+    case LOG_REQUEST:
+      return {
+        ...state,
+        Load: true,
+        Error: false,
+        reg: false,
+      };
+    case LOG_SUCCESS:
+      return {
+        ...state,
+        Load: false,
+        user: payload.userOnline,
+        token: payload.token,
+      };
+    case LOG_FAILURE:
+      return {
+        ...state,
+        Load: false,
+        Error: true,
+      };
+    case REG_REQUEST:
+      return {
+        ...state,
+        Load: true,
+        Error: false,
+        reg: true,
+      };
+    case REG_SUCCESS:
+      return {
+        ...state,
+        Load: false,
+        user: payload.user,
+        token: payload.token,
+      };
+    case REG_FAILURE:
+      return {
+        ...state,
+        Load: false,
+        Error: true,
+      };
+    default:
+      return state;
+  }
+};
--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -19,7 +19,7 @@ const Router = () => {
         <Route path="/login">
           <SignUp />
         </Route>
-        <Route path="/home">
+        <Route path="/wall">
           <Wall />
         </Route>
         <Route path="/friends">
