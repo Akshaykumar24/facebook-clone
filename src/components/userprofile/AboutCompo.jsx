5cd4a707f2d89e314708e5eeaa827c535bc8fd74
Omkar Gavade <omkarvgawade@gmail.com>
2021-09-30 08:09:06 +0530

    working on imagePreview

--- src/components/userprofile/AboutCompo.jsx
+++ src/components/userprofile/AboutCompo.jsx
@@ -5,17 +5,8 @@ import styled from 'styled-components'
 function AboutCompo() {
     return (
         <AboutCompoStlyled>
-            <div className="sideBar">
-                <div>About</div>
-                <div>Overview</div>
-                <div>Work and Education</div>
-                <div>Places lived</div>
-                <div>Contact Basic Info</div>
-                <div>Family and Relationships</div>
-                <div>Details About You</div>
-                <div>Life Events</div>
-            </div>
-            <div className="infosection">
+            <div>
+                <h3>Work</h3>
 
             </div>
 
